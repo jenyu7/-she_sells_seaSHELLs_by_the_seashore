1dd2f18dfa77f5f723cf92ebf54c0484aafe9ef6
jenyu7 <jyu7@stuy.edu>
2017-12-03 23:09:56 -0500

    :zap::zap::zap:

--- shell.c
+++ shell.c
@@ -7,7 +7,12 @@
 
 #include "head.h"
 
-//handle those signals
+/*========static void sighandler(int signo)========
+  Inputs: int signo
+  Outputs: NONE
+
+  If Ctrl-C pressed, print out a new shell prompt.
+  ===============================================*/
 static void sighandler(int signo) {
   if (signo == SIGINT) {
     printf("\n");
@@ -16,17 +21,21 @@ static void sighandler(int signo) {
   }
 }
 
+/*========int main()========
+  Inputs: NONE
+  Outputs: 0
+
+  Keeps the shell running! 
+  Prints prompt, reads input, and executes fxns.
+  =========================*/
 int main()
 {
   signal(SIGINT, sighandler);
   while(1) {
     print_shell_prompt();
-    //printf("reading line\n");
     char * input = read_line();
     exec_all( input );
     free(input);
-    
-    //printf("shell\n");
   }
   return 0;
 }
